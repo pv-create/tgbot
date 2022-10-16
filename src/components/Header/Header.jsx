12a13bb530939ef5a38f6a93ff4e60fef2322274
pavel <pavel_vilkov17@mail.ru>
2022-10-16 09:49:20 +0800

    add new header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,34 @@ const Header = () => {
     const {user, onClose} = useTelegram();
 
     return (
-        <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
-        </div>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Button onClick={onClose}>Закрыть</Button>
+                            </li>
+                        </ul>
+                        <span className={'username'}>
+                             {user?.username}
+                        </span>
+                    </div>
+                </div>
+            </nav>
+        </>
+        // <div className={'header'}>
+        //     <Button onClick={onClose}>Закрыть</Button>
+        //     <span className={'username'}>
+        //         {user?.username}
+        //     </span>
+        // </div>
     );
 };
 
